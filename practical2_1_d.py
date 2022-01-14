2d11eb71c4713d59be5edcaab3916d1f8b58bc23
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-14 14:39:01 +0530

    Add files via upload

---
+++ practical2_1_d.py
@@ -0,0 +1,10 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - A python program to add a key to dictionary.
+Github link - https://github.com/Virti7702/Practical2-1-d.git
+
+'''
+a = {1:1,2:2}
+a.update({3:3})
+print(a)
